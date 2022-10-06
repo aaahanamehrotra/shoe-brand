6155b0cbfe3d9c8627632ec3395a46444b75200b
aaahanamehrotra <e10991aaahana@dpsrkp.net>
2022-10-06 16:14:25 +0530

    products page

---
+++ data/products.js
@@ -0,0 +1,2 @@
+const data = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
+module.exports = data;
---
+++ pages/products.js
@@ -0,0 +1,29 @@
+import styles from "../styles/Product.module.css";
+import data from "../data/products.js";
+import Image from "next/image";
+
+export default function Products() {
+  return (
+    <div className={styles.products}>
+      <div className={styles.cards}>
+        {data.map((product) => (
+          <div className={styles.card}>
+            <Image
+              src="/images/shoe.jpg"
+              width={240}
+              height={240}
+              className={styles.productimage}
+            ></Image>
+            <h2 className={styles.productname}>Lorem Ipsum</h2>
+            <div className={styles.productprice}>$3.99</div>
+            <div className={styles.buttons}>
+              <button className={styles.viewbutton}>View</button>
+              <button className={styles.wishbutton}>WishList</button>
+              <button className={styles.cartbutton}>Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
